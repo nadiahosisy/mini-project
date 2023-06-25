4c10905403a34e6159efaa495dc45ec16e623d39
Your Name <youremail@example.com>
2023-06-25 22:19:42 +0300

    added images

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//import images 
 let img_x = "url('./imge/x.png')";
 let img_o = "url('./imge/o.png')";
 
 function clickBoard(num) {
   let box = document.getElementById(num);
   box.style.backgroundImage = img_x;
+  box.style.backgroundSize = "cover";
 }
